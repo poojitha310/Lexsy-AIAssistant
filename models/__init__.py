96786586da6c7c83f2e0f751e3d121dfcdcde5d0
Poojitha Thatamsetty <61311399+poojitha310@users.noreply.github.com>
2025-08-05 14:48:33 -0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,7 @@ from .client import Client
 from .document import Document
 from .email import Email
 from .conversation import Conversation
+from .chat import router as chat_router
+
 
 __all__ = ["Client", "Document", "Email", "Conversation"]
